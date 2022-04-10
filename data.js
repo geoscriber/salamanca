4b600c14345d8b8353e7faf71c1b6dd6b986bb76
krquinton <nscrexgst.dotr@gmail.com>
2022-04-10 17:05:36 +0800

    initial commit

---
+++ data.js
@@ -0,0 +1,287 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-citta-italia-at-40m",
+      "name": "Citta Italia at 40m",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.5599521727810028,
+        "pitch": 0.6217261331846906,
+        "fov": 1.2820191017947098
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.04786788613219173,
+          "pitch": 0.44647892611479634,
+          "rotation": 0,
+          "target": "1-property-view-front"
+        },
+        {
+          "yaw": 0.18745217206480191,
+          "pitch": 0.35199935441674235,
+          "rotation": 0,
+          "target": "3-property-view-top"
+        },
+        {
+          "yaw": 0.9656656345178085,
+          "pitch": 0.6794477361326354,
+          "rotation": 0,
+          "target": "4-citta-italia-phase-10-at-15m"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.04516883227808499,
+          "pitch": 0.3097213492993003,
+          "title": "Salamanca Property",
+          "text": "<a href='https://youtu.be/d6rEczi52vQ'>Click here to see the aerial video</a>"
+        }
+      ]
+    },
+    {
+      "id": "1-property-view-front",
+      "name": "Property View (Front)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.13300810037288713,
+        "pitch": 0.5656247310326883,
+        "fov": 0.934816720006171
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.31297576984381514,
+          "pitch": 0.10281787458168878,
+          "rotation": 0,
+          "target": "3-property-view-top"
+        },
+        {
+          "yaw": 0.14374889888565967,
+          "pitch": 0.5447167714684085,
+          "rotation": 0,
+          "target": "2-property-view-2-front"
+        },
+        {
+          "yaw": 2.9748247627728874,
+          "pitch": -0.36270299632100844,
+          "rotation": 0,
+          "target": "0-citta-italia-at-40m"
+        },
+        {
+          "yaw": 1.737395886734305,
+          "pitch": -0.0047084096677032505,
+          "rotation": 0,
+          "target": "4-citta-italia-phase-10-at-15m"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-property-view-2-front",
+      "name": "Property View 2 (Front)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.1745329251994292,
+        "pitch": 0.6295535938299004,
+        "fov": 1.2820191017947098
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.9724263978944094,
+          "pitch": -0.5124988435051634,
+          "rotation": 0,
+          "target": "3-property-view-top"
+        },
+        {
+          "yaw": -3.0603870847332892,
+          "pitch": 0.6796555566916087,
+          "rotation": 0,
+          "target": "1-property-view-front"
+        },
+        {
+          "yaw": 2.022886409480148,
+          "pitch": -0.032810675360687824,
+          "rotation": 0,
+          "target": "4-citta-italia-phase-10-at-15m"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-property-view-top",
+      "name": "Property View (Top)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.1463391449749274,
+        "pitch": 0.988223057633455,
+        "fov": 1.2820191017947098
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.8059459089412115,
+          "pitch": 0.7336554280172685,
+          "rotation": 0,
+          "target": "2-property-view-2-front"
+        },
+        {
+          "yaw": -0.545757077224053,
+          "pitch": 0.6301837363839642,
+          "rotation": 0,
+          "target": "1-property-view-front"
+        },
+        {
+          "yaw": -0.7355582645031671,
+          "pitch": -0.39255623253984595,
+          "rotation": 0,
+          "target": "0-citta-italia-at-40m"
+        },
+        {
+          "yaw": -2.3955186496578946,
+          "pitch": -0.02018539742406844,
+          "rotation": 0,
+          "target": "4-citta-italia-phase-10-at-15m"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-citta-italia-phase-10-at-15m",
+      "name": "Citta Italia Phase 10 at 15m",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.653464522622814,
+        "pitch": 0.6470742517487054,
+        "fov": 1.2820191017947098
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.0051217059256778,
+          "pitch": 0.18833980334733447,
+          "rotation": 0,
+          "target": "2-property-view-2-front"
+        },
+        {
+          "yaw": 1.314638828466741,
+          "pitch": 0.10708390958905412,
+          "rotation": 0,
+          "target": "3-property-view-top"
+        },
+        {
+          "yaw": -0.05240106438725789,
+          "pitch": -0.5624800571250255,
+          "rotation": 0,
+          "target": "0-citta-italia-at-40m"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.140757283204346,
+          "pitch": 0.19291526928652303,
+          "title": "Salamanca Property",
+          "text": ""
+        }
+      ]
+    }
+  ],
+  "name": "Citta Italia (Salamanca Propetry)",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
